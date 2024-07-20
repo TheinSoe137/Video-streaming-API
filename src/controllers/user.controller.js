365ec177e5d95fa376c93d6d7cde8346313ab206
PeterSol <thein4edu@gmail.com>
2024-07-20 11:36:09 +0630

    add new controllers using db pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import { User } from "../models/user.model.js";
-import { uploadOnCloud } from "../utils/fileUpload.js";
+import { deleteFromCloud, uploadOnCloud } from "../utils/fileUpload&Delete.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import jwt from "jsonwebtoken";
 const generateTokens = async (userId) => {
@@ -279,6 +279,126 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, user, "coverImage Updated successfully"));
 });
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+
+  if (!username?.trim()) {
+    throw new ApiError(400, "username is missing");
+  }
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username,
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localFiled: "_id",
+        foreignField: "channel",
+        as: "subscribers", //givin own field (this is for subscribers count)
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localFiled: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo", // givin own field (where the user is sub to/channels)
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers", //prefix $ because it is a field
+        },
+        channelSubscribeToCount: {
+          $size: "$subscribedTo", //prefix $ because it is a field,how many channel user have sub to
+        },
+        isSubscribed: {
+          $cond: {
+            //$cond = condition
+            if: {
+              $in: [req.user?._id, "$subscribers.subscriber"],
+              then: true,
+              else: false,
+            },
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullname: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelSubscribeToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      },
+    },
+  ]);
+  if (!channel?.length) {
+    throw new ApiError(501, "channel does not exist");
+  }
+  console.log(channel);
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channel[0], "User channel fetched successfully")
+    );
+});
+
+const getWatchedHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        form: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "wahtchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullname: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: { $first: "$owner" },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, user[0].watchHistory),
+      "watch history fetched successfully"
+    );
+});
 export {
   registerUser,
   loginUser,
@@ -289,4 +409,6 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchedHistory,
 };
---
+++ src/utils/fileUpload&Delete.js
@@ -0,0 +1,42 @@
+// import ImageKit from "imagekit-javascript";
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./env",
+});
+// const imagekit = new ImageKit({
+//   publicKey: process.env.CLOUD_PUBLIC_KEY,
+//   privateKey: process.env.CLOUD_PRIVATE_KEY,
+//   urlEndpoint: process.env.CLOUD_URL_ENDPOINT,
+// });
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
+
+const uploadOnCloud = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfull
+    console.log("file is uploaded on cloudinary ", response.url);
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    // console.log(error, "error in uploding file on cloud"); // remove the locally saved temporary file as the upload operation got failed
+    return error;
+  }
+};
+
+const deleteFromCloud = async (public_id) => {
+  cloudinary.uploader.destroy(public_id).then((result) => console.log(result));
+};
+
+export { uploadOnCloud, deleteFromCloud };
